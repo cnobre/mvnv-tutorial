8d5ccf337460574d565606c72a1261e00a9cbe47
cnobre <carolinanobre@gmail.com>
2019-10-03 20:13:57 -0600

    first version of the make your own graph app. includes back end to use the twitter api and front end to visualize the resulting graph

--- app.js
+++ app.js
@@ -1,118 +1,240 @@
 //Callback functions
-var error = function (err, response, body) {
-    console.log('ERROR [%s]', err);
+var error = function(err, response, body) {
+  console.log("ERROR [%s]", err);
 };
-var success = function (data) {
-    console.log('Data [%s]', data);
+var success = function(data) {
+  console.log("Data [%s]", data);
 };
 
-var Twitter = require('twitter-node-client').Twitter;
+// var Twitter = require("twitter-node-client").Twitter;
 
-//Get this data from your twitter apps dashboard
-// {
-//     "consumerKey": "BH4FzEHkxbKTlbRLeB8zw91Me",
-//     "consumerSecret": "1XSLd2o7SCKtm8D7d1h4HJWAXfeJELji2Bm309xmoutUrynQze",
-//     "accessToken": "739637463900114944-JdoEZqLQuh4CcqECRqpZ5D7xOdkgRTt",
-//     "accessTokenSecret": "gMCGW9KMmXu70WXNMyVzQQiI5Aut3eDKS1zI82aPMK9zS",
-//     "callBackUrl": "http://127.0.0.1:8080"
-// }
+var Twit = require("twit");
 
-// make a directory in the root folder of your project called data
-// copy the node_modules/twitter-node-client/twitter_config file over into data/twitter_config`
-// Open `data/twitter_config` and supply your applications `consumerKey`, 'consumerSecret', 'accessToken', 'accessTokenSecret', 'callBackUrl' to the appropriate fields in your data/twitter_config file
+var T = new Twit({
+  consumer_key: "BH4FzEHkxbKTlbRLeB8zw91Me",
+  consumer_secret: "1XSLd2o7SCKtm8D7d1h4HJWAXfeJELji2Bm309xmoutUrynQze",
+  access_token: "739637463900114944-JdoEZqLQuh4CcqECRqpZ5D7xOdkgRTt",
+  access_token_secret: "gMCGW9KMmXu70WXNMyVzQQiI5Aut3eDKS1zI82aPMK9zS",
+  timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
+  strictSSL: true // optional - requires SSL certificates to be valid.
+});
 
-var twitter;
-// var fs = require('fs');
-//     fs.readFile('data/twitter_config', 'utf8', function(err, contents) {
-//          twitter = new Twitter(JSON.parse(contents));
+//
+//  tweet 'hello world!'
+//
+//   T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
+//     console.log(data)
+//   })
 
+//
+//  search twitter for all tweets containing the word 'banana' since July 11, 2011
+//
 
-//                 //Example calls
+const http = require("http");
+var url = require("url");
 
-//         twitter.getUserTimeline({ screen_name: 'BoyCook', count: '10'}, error, success);
-
-//         twitter.getMentionsTimeline({ count: '10'}, error, success);
-
-//         twitter.getHomeTimeline({ count: '10'}, error, success);
-
-//         twitter.getReTweetsOfMe({ count: '10'}, error, success);
-
-//         twitter.getTweet({ id: '1111111111'}, error, success);
-
-
-//         //
-//         // Get 10 tweets containing the hashtag haiku
-//         //
-
-//         twitter.getSearch({'q':'#haiku','count': 10}, error, success);
-
-//         //
-//         // Get 10 popular tweets with a positive attitude about a movie that is not scary 
-//         //
-
-//         twitter.getSearch({'q':' movie -scary :) since:2013-12-27', 'count': 10, 'result\_type':'popular'}, error, success)
-
-//     });
-
-
-    const http = require('http');
-    var url = require('url');
-
-const hostname = '127.0.0.1';
+const hostname = "0.0.0.0";
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-  res.writeHead(200, {'Content-Type': 'text/html'});
+const server = http.createServer(async (req, res) => {
+  //   res.statusCode = 200;
+  //   res.setHeader('Content-Type', 'text/plain');
+  res.writeHead(200, {
+    "Content-Type": "text/html",
+    "Access-Control-Allow-Origin": "*"
+  });
 
   var q = url.parse(req.url, true).query;
   var handle = q.handle;
 
-  var twitter;
-var fs = require('fs');
-    fs.readFile('data/twitter_config', 'utf8', function(err, contents) {
-         twitter = new Twitter(JSON.parse(contents));
+  let graph = { nodes: [], links: [] };
 
+  let numTweets = 5;
 
-                //Example calls
-        let answer = twitter.getUserTimeline({ screen_name:handle, count: '1'}, error, success)
-        res.end(answer);
+  function addNode(user) {
+    //check if source node exists
+    let node = graph.nodes.find(n => n.name == user.screen_name);
+    if (!node) {
+      graph.nodes.push({
+        name: user.screen_name,
+        group: Math.random() > 0.5 ? 4 : 1,
+        friends: user.friends_count  || 0,
+        followers: user.followers_count || 0,
+        location:user.location || 'NA'
+      });
+    //   console.log(user.screen_name,user.friends_count,user.followers.count)
+    }
+    return;
+  }
 
-        // twitter.getMentionsTimeline({ count: '10'}, error, success);
+  function addEdge(source, target, type) {
+    let sourceNode = graph.nodes.find(n => n.name == source);
+    let targetNode = graph.nodes.find(n => n.name == target);
 
-        // twitter.getHomeTimeline({ count: '10'}, error, success);
+    let sourceIndex = graph.nodes.indexOf(sourceNode);
+    let targetIndex = graph.nodes.indexOf(targetNode);
+    let edge = graph.links.find(
+      e => e.source == sourceIndex && e.target == targetIndex && e.type == type
+    );
 
-        // twitter.getReTweetsOfMe({ count: '10'}, error, success);
+    if (edge) {
+      //edge exists, increase count
+      edge.weight = edge.weight + 1;
+    } else {
+      //create new edge
+      graph.links.push({
+        source: sourceIndex,
+        target: targetIndex,
+        weight: 1,
+        type
+      });
+    }
+  }
 
-        // twitter.getTweet({ id: '1111111111'}, error, success);
+  function parse_tweets(tweets) {
+    if (tweets.length == 0) {
+      console.log("no tweets found");
+      return;
+    }
+    //only adds user node if it don't already exist
+    addNode(tweets[0].user);
 
+    console.log(tweets[0].user.friends_count,tweets[0].user.followers_count)
 
-        //
-        // Get 10 tweets containing the hashtag haiku
-        //
+    tweets.map(tweet => {
+      //check if it's  a retweet:
+      if (tweet.retweeted_status) {
+        addNode(tweet.retweeted_status.user);
+        addEdge(
+          tweet.user.screen_name,
+          tweet.retweeted_status.user.screen_name,
+          "retweet"
+        );
+      } else {
+        //check for quoted:
+        if (tweet.quoted_status) {
+          addNode(tweet.quoted_status.user);
+          addEdge(
+            tweet.user.screen_name,
+            tweet.quoted_status.user.screen_name,
+            "quote"
+          );
+        }
+        //check for user mentions
+        if (tweet.entities.user_mentions) {
+          tweet.entities.user_mentions.map(m => {
+            addNode(m);
+            addEdge(tweet.user.screen_name, m.screen_name, "mention");
+          });
+        }
+      }
+    });
+  }
 
-        // twitter.getSearch({'q':'#haiku','count': 10}, error, success);
+  var fs = require("fs");
 
-        //
-        // Get 10 popular tweets with a positive attitude about a movie that is not scary 
-        //
+  let tweets = await T.get("statuses/user_timeline", {
+    screen_name: handle,
+    count: numTweets
+  });
 
-        // twitter.getSearch({'q':' movie -scary :) since:2013-12-27', 'count': 10, 'result\_type':'popular'}, error, success)
+  console.log("getting tweets for ", handle);
 
+  parse_tweets(tweets.data);
+
+  //create copy of seed edges to iterate through;
+  let seed_edges = JSON.parse(JSON.stringify(graph.links));
+
+  //   console.log(seed_edges);
+  //for each user neighbor, get their neighbors;
+  let edgeMap = seed_edges.map(async edge => {
+    //   console.log('target is ', edge.target)
+    let tweets = await T.get("statuses/user_timeline", {
+      screen_name: graph.nodes[edge.target].name,
+      count: numTweets
     });
 
+    console.log("getting tweets for ", graph.nodes[edge.target].name);
+    
 
-//  console.log(txt)
-//   res.end(txt);
+    parse_tweets(tweets.data);
+  });
+
+  Promise.all(edgeMap).then(d => {
+    console.log("done with promises");
+    //   console.log(graph)
+    res.end(JSON.stringify(graph));
+  });
+
+  fs.writeFileSync(
+    "tweets_" + handle + ".json",
+    JSON.stringify(tweets, null, 4)
+  );
+
+  //   fs.readFile("data/twitter_config", "utf8", function(err, contents) {
+  //     twitter = new Twitter(JSON.parse(contents));
+
+  //     //Example calls
+  //     twitter.getUserTimeline(
+  //       { screen_name: handle, count: "5" },
+  //       (error = () => res.end(JSON.stringify({ data: "problem" }))),
+  //       (success = data => {
+  //         //Iterate through all the mentions;
+
+  //         let tweets = JSON.parse(data);
+  //         let edges = parse_tweets(tweets);
+
+  //         edges.filter(e=>e.type === "mention").map(mention => {
+  //           twitter.getUserTimeline(
+  //             { screen_name: mention.target, count: "1" },
+  //             (error = () => res.end(JSON.stringify({ data: "problem" }))),
+  //             (success = data => {
+  //               let edges = parse_tweets(JSON.parse(data));
+  //               console.log("parsed tweets for", mention.target, edges);
+  //             })
+  //           );
+  //         });
+
+  //         // for each mention get their latest 5 tweets and who they mention/etc...
+
+  //         // fs.writeFileSync(
+  //         //   "tweets.json",
+  //         //   JSON.stringify(JSON.parse(data), null, 4)
+  //         // );
+
+  //         res.end(JSON.stringify({ edges }));
+  //       })
+  //     );
+
+  //     // res.end(JSON.stringify({data:answer}))
+
+  //     // twitter.getMentionsTimeline({ count: '10'}, error, success);
+
+  //     // twitter.getHomeTimeline({ count: '10'}, error, success);
+
+  //     // twitter.getReTweetsOfMe({ count: '10'}, error, success);
+
+  //     // twitter.getTweet({ id: '1111111111'}, error, success);
+
+  //     //
+  //     // Get 10 tweets containing the hashtag haiku
+  //     //
+
+  //     // twitter.getSearch({'q':'#haiku','count': 10}, error, success);
+
+  //     //
+  //     // Get 10 popular tweets with a positive attitude about a movie that is not scary
+  //     //
+
+  //     // twitter.getSearch({'q':' movie -scary :) since:2013-12-27', 'count': 10, 'result\_type':'popular'}, error, success)
+  //   });
+
+  //  console.log(txt)
+  //   res.end(txt);
 });
-
-
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-
 // var twitter = new Twitter();
-
